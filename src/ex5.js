1a397e0822327515cf61a43b379caace9ee8c5ea
Marcin Grygierek <grygierek.marcin@gmail.com>
2021-11-26 11:54:29 +0100

    Ex5 - interfaces, optional properties

---
+++ src/ex5.js
@@ -0,0 +1,31 @@
+var user = {
+    name: 'Marcin',
+    age: 28,
+    addresses: [
+        {
+            city: 'Gliwice',
+            street: 'Dworcowa'
+        }
+    ]
+};
+var user2 = {
+    name: 'Marcin',
+    age: 28,
+    height: 180,
+    addresses: [
+        {
+            street: 'Dworcowa',
+            city: 'Gliwice'
+        }
+    ]
+};
+var user3 = {
+    name: 'Tomasz',
+    age: 48,
+    addresses: [
+        {
+            street: 'Lorem',
+            city: 'Ipsum'
+        }
+    ]
+};
